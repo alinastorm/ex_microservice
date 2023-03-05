d2ecbfb36bc0c140065d713a2322e67c5c7c7acf
AlexGr <753464@gmail.com>
2023-03-05 12:04:34 +0300

    1step

--- my_rest_api/app.js
+++ my_rest_api/app.js
@@ -1,41 +1,27 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const express = require('express')
+const ExpressPinoLogger = require('express-pino-logger')
+const cookieParser = require('cookie-parser')
+const pino = ExpressPinoLogger({
+    serializers: {
+        req: (req) => ({
+            method: req.method,
+            url: req.url,
+            user: req.raw.user,
+        }),
+    },
+})
+const app = express()
+app.use(pino)
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use('/', require('./routes'))
+app.use((req, res, next) => {
+    res.status(404)
+        .json({
+            code: 404,
+            title: `That resource  "${req.url}"  was not found`,
+            description: `Ресурс "${req.url}" не найден`
+        })
+})
+module.exports = app
